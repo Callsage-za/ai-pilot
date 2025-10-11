0bddfba0eabdd6248e866ce8d076628c72b8ca49
Jackson <jackson@balanceapp.co.za>
2025-10-11 18:23:30 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS handled by Apache reverse proxy
+  // Override Apache CORS headers
+  app.use((req, res, next) => {
+    // Remove conflicting headers
+    res.removeHeader('Access-Control-Allow-Origin');
+    res.removeHeader('Access-Control-Allow-Credentials');
+    
+    // Set correct CORS headers
+    res.header('Access-Control-Allow-Origin', 'https://callsage.balanceapp.co.za');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Origin, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400');
+    
+    next();
+  });
 
   await app.listen(process.env.PORT ?? 8787);
 }
